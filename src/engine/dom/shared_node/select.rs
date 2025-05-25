ae667aa46e643db1d0be40c0a26cf6e4350ecec7
Simone Tesini <simozilla006@gmail.com>
2025-05-25 18:29:23 +0200

    refactor select

--- src/engine/dom/shared_node/select.rs
+++ src/engine/dom/shared_node/select.rs
@@ -9,11 +9,7 @@ use super::SharedNode;
 
 pub trait Select {
     fn query_select(&self, query: &str) -> anyhow::Result<Vec<SharedNode>>;
-    fn select(&self, _selector: &stylesheet::ComplexSelector) -> anyhow::Result<Vec<SharedNode>>;
-    fn select_no_recursive(
-        &self,
-        _selector: &stylesheet::ComplexSelector,
-    ) -> anyhow::Result<Vec<SharedNode>>;
+    fn select(&self, selector: &stylesheet::ComplexSelector) -> anyhow::Result<Vec<SharedNode>>;
 }
 
 impl Select for SharedNode {
@@ -22,37 +18,68 @@ impl Select for SharedNode {
     }
 
     fn select(&self, selector: &stylesheet::ComplexSelector) -> anyhow::Result<Vec<SharedNode>> {
-        let self_lock = self.read().unwrap();
+        let _self_lock = self.read().unwrap();
         let mut candidates = vec![];
 
-        // TODO: for tidiness purposes, make a second trait for all select helpers
-        // for example, this would be a recursive selection
-        // so that it can be reused later for the descendant combinator
-
         // for selector in selector.inner { // TEMP:
-        let simple = selector.inner[0].clone(); // TEMP:
+        let simple = &selector.inner[0]; // TEMP:
+        candidates.extend(self.select_simple_recursive(simple)?);
+
+        Ok(candidates)
+    }
+}
+
+trait SelectHelper {
+    fn select_simple_recursive(
+        &self,
+        simple: &stylesheet::Selector,
+    ) -> anyhow::Result<Vec<SharedNode>>;
+    fn select_simple_no_recursive(
+        &self,
+        simple: &stylesheet::Selector,
+    ) -> anyhow::Result<Vec<SharedNode>>;
+}
+
+impl SelectHelper for SharedNode {
+    fn select_simple_recursive(
+        &self,
+        simple: &stylesheet::Selector,
+    ) -> anyhow::Result<Vec<SharedNode>> {
+        let self_lock = self.read().unwrap();
+        let mut candidates = vec![];
 
         for child in self_lock.children.iter() {
             let child_lock = child.read().unwrap(); // unwrap on poison
 
             // if it isnt an element, we don't even want to match it, so completely ignore it
             if let NodeType::Element(element) = &child_lock.node_type {
-                if element.matches_selector(&simple) {
+                if element.matches_selector(simple) {
                     candidates.push(Arc::clone(child));
                 }
 
-                candidates.extend(child.select(selector)?);
+                candidates.extend(child.select_simple_recursive(simple)?);
             }
         }
-        // }
-
         Ok(candidates)
     }
 
-    fn select_no_recursive(
+    fn select_simple_no_recursive(
         &self,
-        _selector: &stylesheet::ComplexSelector,
+        simple: &stylesheet::Selector,
     ) -> anyhow::Result<Vec<SharedNode>> {
-        Ok(vec![])
+        let self_lock = self.read().unwrap();
+        let mut candidates = vec![];
+
+        for child in self_lock.children.iter() {
+            let child_lock = child.read().unwrap(); // unwrap on poison
+
+            // if it isnt an element, we don't even want to match it, so completely ignore it
+            if let NodeType::Element(element) = &child_lock.node_type {
+                if element.matches_selector(simple) {
+                    candidates.push(Arc::clone(child));
+                }
+            }
+        }
+        Ok(candidates)
     }
 }
